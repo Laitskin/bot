09717638024f7e82f6ad194b1e81372d5b15455e
sebastian-starck <sebastian@starck.im>
2021-04-12 12:32:35 -0300

    Updated main & libs

--- main.ts
+++ main.ts
@@ -1,42 +1,31 @@
+import express from "express";
+
 import { SessionService } from "./src/session/SessionService";
 import { DatabaseService } from "./src/database/DatabaseService";
-import express from "express";
 import { DiscordService } from "./src/discord/DiscordService";
-import { ListPrettifier } from "./src/util/ListPrettifier";
+import { DatabaseTable } from "./src/database/types";
+import { Client, ClientEvents } from "discord.js";
+import {LogEvent, Logger} from "./src/Logger";
 
 export let databaseService: DatabaseService;
 export let sessionService: SessionService;
 export let discordService: DiscordService;
-export let listPrettifier: ListPrettifier;
 
 let app;
 let listener;
 
-function init() {
-  console.log("Starting services");
-  databaseService = new DatabaseService();
-  sessionService = new SessionService();
-  discordService = new DiscordService();
-  listPrettifier = new ListPrettifier();
+async function init() {
+  databaseService = new DatabaseService().init();
+  sessionService = new SessionService().init();
+  discordService = new DiscordService().init();
 
-  console.log("Starting server");
+  Logger.logEvent(LogEvent.BOOTSTRAP);
   app = express();
-  listener = app.listen(process.env.PORT, () => {
-    console.log("Your app is listening on port " + listener.address().port);
+  listener = app.listen(process.env.PORT, () => {});
+
+  await discordService.mockCommand("-leaderboard", {
+    userName: "Revan",
   });
-
-  discordService.init();
-
-  // discordService.mockCommand("-help");
-  const list = listPrettifier.prettify([
-    {
-      userName: "lait",
-      petePuntos: -1000,
-      creditos: 30,
-    },
-  ]);
-
-  console.log(list);
 }
 
 init();
